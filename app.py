96e030e749fe634f0de7c4fae07fbb685c590963
Agus Setiawan <kyasa422@gmail.com>
2024-07-10 21:45:06 +0700

    service

--- app.py
+++ app.py
@@ -2,21 +2,20 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import pandas as pd
 import joblib
+from mlxtend.frequent_patterns import apriori, association_rules
 
 app = FastAPI()
 
 # Load model
-model = joblib.load('model.pkl')
+model = joblib.load('amodel.pkl')
+rules = joblib.load('rmodel.pkl')    
 
-# Pydantic model untuk validasi input data
-class MenuItem(BaseModel):
-    id: int
-    menu: str
+
 
 class AprioriRequest(BaseModel):
-    data: list[MenuItem]
-    support: float = 0.01
-    min_threshold: float = 1
+   
+    input_item : str
+
 
 # Fungsi untuk preprocessing data
 def preprocess_data(df):
@@ -26,29 +25,35 @@ def preprocess_data(df):
     item_count_pivot = item_count_pivot.applymap(lambda x: 1 if x >= 1 else 0)
     return item_count_pivot
 
+def build_apriori_model(item_count_pivot, support=0.01, metric="lift", min_threshold=1):
+    frequent_items = apriori(item_count_pivot, min_support=support, use_colnames=True)
+    rules = association_rules(frequent_items, metric=metric, min_threshold=min_threshold)[["antecedents", "consequents", "support", "confidence", "lift"]]
+    return rules
+
+def recommend_menu(rules, input_item):
+    filtered_rules = rules[rules['antecedents'].apply(lambda x: input_item in x)]
+    if not filtered_rules.empty:
+        max_confidence_rule = filtered_rules.loc[filtered_rules['confidence'].idxmax()]
+        recommendations = max_confidence_rule[['antecedents', 'consequents', 'support', 'confidence', 'lift']].to_dict()
+        return recommendations
+    else:
+        return {"message": "No recommendations found."}
+    # filtered_rules = filtered_rules.sort_values(by='confidence', ascending=False)
+    # recommendations = filtered_rules[['antecedents', 'consequents', 'support', 'confidence', 'lift']]
+    # return recommendations
+
 @app.get("/")
 def read_root():
     return {"message": "Sentiment Analysis API is running!"}
 
 @app.post("/apriori")
-async def apriori_api(request: AprioriRequest):
-    try:
-        # Ambil data dari permintaan
-        data = request.data
-        df = pd.DataFrame([item.dict() for item in data])
-        
-        # Pra-pemrosesan data
-        item_count_pivot = preprocess_data(df)
-        
-        # Menggunakan model untuk membuat prediksi atau menghasilkan output
-        support = request.support
-        min_threshold = request.min_threshold
-        
-        filtered_model = model[(model['support'] >= support) & (model['lift'] >= min_threshold)]
-        
-        return filtered_model.to_dict(orient='records')
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
+async def apriori_api( request: AprioriRequest):
+    
+    input_item = request.input_item
+    recomendasi = recommend_menu(rules, input_item)
+    return recomendasi
+     
+   
 
 if __name__ == '__main__':
     import uvicorn
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+   #   text = request.text
+    # text_vector = vectorizer.transform([text])
+    # prediction = model.predict(text_vector)
+    # sentiment = "positive" if prediction[0] == 1 else "negative"
+    # return {"prediction": sentiment}
