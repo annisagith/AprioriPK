02952277c5de52a7915e9b16d60dbc8230acfa77
Agus Setiawan <kyasa422@gmail.com>
2024-07-15 21:50:39 +0700

    finis

--- app.py
+++ app.py
@@ -2,12 +2,12 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import pandas as pd
 import joblib
-from mlxtend.frequent_patterns import apriori, association_rules
+
 
 app = FastAPI()
 
 # Load model
-# model = joblib.load('amodel.pkl')
+model = joblib.load('amodel.pkl')
 rules = joblib.load('rmodel.pkl')    
 
 
@@ -18,12 +18,12 @@ class AprioriRequest(BaseModel):
 
 
 # Fungsi untuk preprocessing data
-# def preprocess_data(df):
-#     item_count = df.groupby(["id", "menu"])["menu"].count().reset_index(name="count")
-#     item_count_pivot = item_count.pivot_table(index='id', columns='menu', values='count', aggfunc='sum').fillna(0)
-#     item_count_pivot = item_count_pivot.astype("int32")
-#     item_count_pivot = item_count_pivot.applymap(lambda x: 1 if x >= 1 else 0)
-#     return item_count_pivot
+def preprocess_data(df):
+    item_count = df.groupby(["id", "menu"])["menu"].count().reset_index(name="count")
+    item_count_pivot = item_count.pivot_table(index='id', columns='menu', values='count', aggfunc='sum').fillna(0)
+    item_count_pivot = item_count_pivot.astype("int32")
+    item_count_pivot = item_count_pivot.applymap(lambda x: 1 if x >= 1 else 0)
+    return item_count_pivot
 
 # def build_apriori_model(item_count_pivot, support=0.01, metric="lift", min_threshold=1):
 #     frequent_items = apriori(item_count_pivot, min_support=support, use_colnames=True)
@@ -44,7 +44,7 @@ def recommend_menu(rules, input_item):
 
 @app.get("/")
 def read_root():
-    return {"message": "rekomendasi API is running!"}
+    return {"message": "Sentiment Analysis API is running!"}
 
 @app.post("/apriori")
 async def apriori_api( request: AprioriRequest):
@@ -58,4 +58,3 @@ async def apriori_api( request: AprioriRequest):
 if __name__ == '__main__':
     import uvicorn
     uvicorn.run(app)
-
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,5 +1,4 @@
-   #   text = request.text
-    # text_vector = vectorizer.transform([text])
-    # prediction = model.predict(text_vector)
-    # sentiment = "positive" if prediction[0] == 1 else "negative"
-    # return {"prediction": sentiment}
+def build_apriori_model(item_count_pivot, support=0.01, metric="lift", min_threshold=1):
+#     frequent_items = apriori(item_count_pivot, min_support=support, use_colnames=True)
+#     rules = association_rules(frequent_items, metric=metric, min_threshold=min_threshold)[["antecedents", "consequents", "support", "confidence", "lift"]]
+#     return rules
