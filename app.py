9d19ccf0275669fe8081589719514aed26eb7b37
Agus Setiawan <kyasa422@gmail.com>
2024-07-17 17:05:13 +0700

    revisi cors

--- app.py
+++ app.py
@@ -2,9 +2,24 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import pandas as pd
 import joblib
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
+# Konfigurasi CORS
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Load model
 
